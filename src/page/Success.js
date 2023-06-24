ac5d8b4353bc643572883717113aafb0f805d777
AdiriOghenetega <tegaadiri3@yahoo.com>
2023-06-24 23:00:25 +0100

    updated success.js

--- src/page/Success.js
+++ src/page/Success.js
@@ -2,15 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Success = () => {
-  const location = localStorage.getItem("location")
+
   return (
     <div>
       <div className="bg-green-200 w-full max-w-md m-auto h-36 flex justify-center items-center font-semibold text-lg mt-6">
         <p>Payment was Successfully</p>
       </div>
       <div className="flex flex-col justify-center items-center">
-      <Link to={`/menu/${location}`}>
-        <button className="w-fit m-auto  bg-[rgb(233,142,30)] hover:bg-orange-600 cursor-pointer text-white text-center p-2 rounded mt-4">
+      <Link to={`/`}>
+        <button className="w-fit m-auto bg-red-900 hover:bg-red-600 cursor-pointer text-white text-center p-2 rounded mt-4">
           Continue Shopping
         </button>
       </Link>
